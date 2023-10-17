14898de899428646a0ae1ac3c53d6911c97a82e9
Vitor Garcia de Vasconcelos <vitorgarciadevasconcelos@gmail.com>
2023-10-17 10:55:12 -0300

    Mudança nos insert do SQL

--- src/main/resources/script/insert.sql
+++ src/main/resources/script/insert.sql
@@ -1,27 +1,28 @@
-INSERT INTO backend.produto (nome, fabricante, valor, peso, data_cadastro)
-VALUES('Café', 'Ouro', 12.5, 0.5, sysdate());
+INSERT INTO backend.fabricantes
+ (cnpj, CEP, UF, Cidade, nome)
+ VALUES('04818506000222', '88075260', 'SC', 'Florianopolis', 'Pera');
+
+INSERT INTO backend.fabricantes
+ (cnpj, CEP, UF, Cidade, nome)
+ VALUES('04818506000444', '88495000', 'SC', 'Garopaba', 'Melita');
+
+INSERT INTO backend.fabricantes
+ (cnpj, CEP, UF, Cidade, nome)
+ VALUES('04818506000163', '88495000', 'SC', 'Garopaba', 'Raider');
+
+INSERT INTO backend.fabricantes
+ (cnpj, CEP, UF, Cidade, nome)
+ VALUES('04818506000555', '88495000', 'SC', 'Garopaba', 'Bisnaguinha');
+ 
+INSERT INTO backend.produto
+ (nome, fabricante , valor, peso, data_cadastro)
+ VALUES('Chinela', "Raider", 50, 0.5, '2023-10-10');
 
 INSERT INTO backend.produto (nome, fabricante, valor, peso, data_cadastro)
 VALUES('Chinela', 'Rasteiras de madeira', 80.5, 0.2, sysdate());
 
-INSERT INTO backend.produto
- (nome, fabricante , valor, peso, data_cadastro)
- VALUES('Chinela', "Raider", 50, 1, '2023-10-10');
- 
- INSERT INTO backend.fabricantes
- (cnpj, CEP, UF, Cidade, nome)
- VALUES('04818506000163', '88495000', 'SC', 'Garopaba', 'Raider');
+INSERT INTO backend.produto (nome, fabricante, valor, peso, data_cadastro)
+VALUES('Café', 'Melita', 12.5, 0.5, sysdate());
 
--- INSERT INTO backend.fabricantes
--- (cnpj, CEP, UF, Cidade, nome)
--- VALUES('', '', '', '', '');
-
-
--- ALTER TABLE backend.produto CHANGE id_fabricante fabricante VARCHAR(255) NOT NULL;
--- ALTER TABLE backend.produto MODIFY COLUMN fabricante VARCHAR(255) NOT NULL;
-
-ALTER TABLE backend.produto CHANGE id_fabricante fabricante VARCHAR(255) NOT NULL;
-ALTER TABLE backend.produto MODIFY COLUMN fabricante VARCHAR(255) NOT NULL;
-
-ALTER TABLE backend.produto ADD CONSTRAINT produto_FK FOREIGN KEY (fabricante) REFERENCES backend.fabricantes(id);
-ALTER TABLE backend.produto ADD CONSTRAINT produto_FK FOREIGN KEY (fabricante) REFERENCES backend.fabricantes(id);
+INSERT INTO backend.produto (nome, fabricante, valor, peso, data_cadastro)
+VALUES('Pão', 'Bisnaguinha', 15, 1, sysdate());
