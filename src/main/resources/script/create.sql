06482e0fa32a027f83748fad3b46ecaa11288a39
Vitor Garcia de Vasconcelos <vitorgarciadevasconcelos@gmail.com>
2023-10-19 10:10:45 -0300

    Mudanças no script do banco de dados

--- src/main/resources/script/create.sql
+++ src/main/resources/script/create.sql
@@ -1,42 +1,20 @@
-CREATE TABLE backend.produto (
-	CREATE TABLE `produtos` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(400) NOT NULL,
-  `id_fabricante` int NOT NULL,
-  `valor` decimal(10,0) NOT NULL,
-  `peso` decimal(10,0) NOT NULL,
-  `data_cadastro` date NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_fabricante` (`id_fabricante`),
-  CONSTRAINT `id_fabricante` FOREIGN KEY (`id_fabricante`) REFERENCES `fabricantes` (`id`)
-  
-  CREATE TABLE backend.produto (
-    id_produto INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
-    id_fabricante INT NOT NULL,
-    valor DECIMAL(10, 2) NOT NULL,
-    peso DECIMAL(8, 2) NOT NULL,
-    data_cadastro DATE NOT NULL
-);
-
-CREATE TABLE backend.fabricantes (
-    cnpj VARCHAR(14) NOT NULL,
-    CEP VARCHAR(9) NOT NULL,
-    UF CHAR(2) NOT NULL,
-    Cidade VARCHAR(255) NOT NULL,
-    nome VARCHAR(255) NOT NULL
-);
-  
-);
-
-CREATE TABLE backend.fabricantes (
+CREATE TABLE backend.fabricante (
 	id int auto_increment NOT NULL,
+	nome varchar(400) NOT NULL,
 	cnpj varchar(14) NOT NULL,
+	cep varchar(14) NOT NULL,
+	cidade varchar(100) NOT NULL,
+	uf varchar(2) NOT null,
 	CONSTRAINT fabricantes_pk PRIMARY KEY (id)
-	
+);
 
-ALTER TABLE backend.fabricantes ADD CEP varchar(12) NOT NULL;
-ALTER TABLE backend.fabricantes ADD Cidade varchar(255) NOT NULL;
-ALTER TABLE backend.fabricantes ADD UF varchar(2) NOT NULL;
-
-ALTER TABLE backend.produto  ADD fabricante varchar(100) NOT NULL;
+CREATE TABLE backend.produto (
+	id int auto_increment NOT NULL,
+	nome varchar(400) NOT NULL,
+	id_fabricante int NOT NULL,
+	valor DECIMAL NOT NULL,
+	peso DECIMAL NOT NULL,
+	data_cadastro DATE NOT NULL,
+	CONSTRAINT produtos_pk PRIMARY KEY (id),
+	CONSTRAINT id_fabricante foreign key (id_fabricante) references backend.fabricante(ID)
+);
--- src/main/resources/script/insert.sql
+++ src/main/resources/script/insert.sql
@@ -1,28 +1,33 @@
-INSERT INTO backend.fabricantes
- (cnpj, CEP, UF, Cidade, nome)
- VALUES('04818506000222', '88075260', 'SC', 'Florianopolis', 'Pera');
 
-INSERT INTO backend.fabricantes
+ INSERT INTO backend.fabricante
+ (cnpj, CEP, UF, Cidade, nome)
+ VALUES('04818506000222', '88075260', 'SC', 'Florianopolis', 'Adidas');
+
+INSERT INTO backend.fabricante
  (cnpj, CEP, UF, Cidade, nome)
  VALUES('04818506000444', '88495000', 'SC', 'Garopaba', 'Melita');
 
-INSERT INTO backend.fabricantes
+INSERT INTO backend.fabricante
  (cnpj, CEP, UF, Cidade, nome)
  VALUES('04818506000163', '88495000', 'SC', 'Garopaba', 'Raider');
 
-INSERT INTO backend.fabricantes
+INSERT INTO backend.fabricante
  (cnpj, CEP, UF, Cidade, nome)
- VALUES('04818506000555', '88495000', 'SC', 'Garopaba', 'Bisnaguinha');
+ VALUES('04818506000555', '88495000', 'SC', 'Garopaba', 'Bauduco');
  
 INSERT INTO backend.produto
- (nome, fabricante , valor, peso, data_cadastro)
- VALUES('Chinela', "Raider", 50, 0.5, '2023-10-10');
+ (nome, id_fabricante , valor, peso, data_cadastro)
+ VALUES('Sandália', 1, 50, 0.5, sysdate());
 
-INSERT INTO backend.produto (nome, fabricante, valor, peso, data_cadastro)
-VALUES('Chinela', 'Rasteiras de madeira', 80.5, 0.2, sysdate());
+INSERT INTO backend.produto (nome, id_fabricante, valor, peso, data_cadastro)
+VALUES('Café', 2, 12.5, 0.5, sysdate());
 
-INSERT INTO backend.produto (nome, fabricante, valor, peso, data_cadastro)
-VALUES('Café', 'Melita', 12.5, 0.5, sysdate());
+INSERT INTO backend.produto (nome, id_fabricante, valor, peso, data_cadastro)
+VALUES('Chinela', 3, 80.5, 0.2, sysdate());
 
-INSERT INTO backend.produto (nome, fabricante, valor, peso, data_cadastro)
-VALUES('Pão', 'Bisnaguinha', 15, 1, sysdate());
+INSERT INTO backend.produto (nome, id_fabricante, valor, peso, data_cadastro)
+VALUES('Pão', 4, 15, 1, sysdate());
+
+select * from fabricante f ;
+
+select * from produto p ;
